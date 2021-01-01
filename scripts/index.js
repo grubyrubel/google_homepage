ed84fc73332ab5237d95d61752387f8b03c3d9d7
grubyrubel <oohyr1@gmail.com>
2021-01-02 00:40:59 +0100

    in serachpage the navogation bar is ready, I added also results section

--- scripts/index.js
+++ scripts/index.js
@@ -33,49 +33,9 @@ button_clear.onclick = function clearfields (){
 };
 
 document.getElementById('button1').onclick=function(){
-  location.href="https://grubyrubel.github.io/google_homepage/search";
-};
-
-document.getElementById('button2').onclick=function(){
-  location.href="https://www.google.com/doodles/";
+  location.href="search.html";
 };
 
 document.getElementById('acount').onclick=function(){
   location.href="https://github.com/grubyrubel";
-};
-
-document.getElementById('top_buttons1').onclick=function(){
-  location.href="https://accounts.google.com/";
-};
-
-document.getElementById('top_buttons2').onclick=function(){
-  location.href="https://www.google.pl/imghp?hl=en&tab=wi&ogbl";
-};
-
-document.getElementById('advertising').onclick=function(){
-  location.href="https://ads.google.com/";
-};
-
-document.getElementById('business').onclick=function(){
-  location.href="https://www.google.com/services/";
-};
-
-document.getElementById('about').onclick=function(){
-  location.href="https://about.google/";
-};
-
-document.getElementById('how_search_works').onclick=function(){
-  location.href="https://www.google.com/search/howsearchworks/";
-};
-
-document.getElementById('privacy').onclick=function(){
-  location.href="https://policies.google.com/privacy";
-};
-
-document.getElementById('terms').onclick=function(){
-  location.href="https://policies.google.com/terms";
-};
-
-document.getElementById('settings').onclick=function(){
-  location.href="https://www.google.com/preferences";
 };
--- scripts/search.js
+++ scripts/search.js
@@ -3,21 +3,7 @@ var searchbox_prevent=document.getElementById('searchbox');
 var button_clear=document.getElementById('button');
 var bar_clear=document.getElementById('bar');
 var magnifier=document.getElementById('magnifier');
-
-document.addEventListener('click', function(event) {
-  var isClickInside = text_field_prevent.contains(event.target);
-
-  if (!isClickInside) {
-    
-    magnifier.className="magnifier-non";
-    searchbox_prevent.className="searchbox-non";
-
-  }else{
-    
-    magnifier.className="magnifier";
-    searchbox_prevent.className="searchbox";
-  }
-});
+var magnifier_blue=document.getElementById('magnifier_blue');
 
 searchbox_prevent.onclick=function(){
   text_field_prevent.focus();
@@ -41,111 +27,218 @@ button_clear.onclick = function clearfields (){
   bar_clear.style.visibility="hidden";
 };
 
-document.getElementById('magnifier_blue').onclick=function(){
-  location.href="";
-};
-
 document.getElementById('acount').onclick=function(){
   location.href="https://github.com/grubyrubel";
 };
 
 document.getElementById('first_label').className="bar_lower1";
+document.getElementById('b1').src="images/all.png";
 
 document.getElementById('first_label').onclick=function(){
     document.getElementById('first_label').className="bar_lower1";
     document.getElementById('second_label').className="bar_lower";
     document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
     document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
+    document.getElementById('b1').src="images/all.png";
+    document.getElementById('b2').src="images/images.png";
+    document.getElementById('b3').src="images/videos.png";
+    document.getElementById('b5').src="images/news.png";
 };
 
 document.getElementById('second_label').onclick=function(){
     document.getElementById('first_label').className="bar_lower";
     document.getElementById('second_label').className="bar_lower1";
     document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
     document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
-
+    document.getElementById('b1').src="images/allb.png";
+    document.getElementById('b2').src="images/imagesk.png";
+    document.getElementById('b3').src="images/videos.png";
+    document.getElementById('b5').src="images/news.png";
 };
 
 document.getElementById('third_label').onclick=function(){
     document.getElementById('first_label').className="bar_lower";
     document.getElementById('second_label').className="bar_lower";
     document.getElementById('third_label').className="bar_lower1";
-    document.getElementById('fourth_label').className="bar_lower";
     document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
-
-};
-
-document.getElementById('fourth_label').onclick=function(){
-    document.getElementById('first_label').className="bar_lower";
-    document.getElementById('second_label').className="bar_lower";
-    document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower1";
-    document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
-
+    document.getElementById('b1').src="images/allb.png";
+    document.getElementById('b2').src="images/images.png";
+    document.getElementById('b3').src="images/videosk.png";
+    document.getElementById('b5').src="images/news.png";
 };
 
 document.getElementById('fiveth_label').onclick=function(){
     document.getElementById('first_label').className="bar_lower";
     document.getElementById('second_label').className="bar_lower";
     document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
     document.getElementById('fiveth_label').className="bar_lower1";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
-
+    document.getElementById('b1').src="images/allb.png";
+    document.getElementById('b2').src="images/images.png";
+    document.getElementById('b3').src="images/videos.png";
+    document.getElementById('b5').src="images/newsk.png";
 };
 
-document.getElementById('sixth_label').onclick=function(){
-    document.getElementById('first_label').className="bar_lower";
-    document.getElementById('second_label').className="bar_lower";
-    document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
-    document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower1";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower";
 
-};
 
-document.getElementById('seventh_label').onclick=function(){
-    document.getElementById('first_label').className="bar_lower";
-    document.getElementById('second_label').className="bar_lower";
-    document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
-    document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower1";
-    document.getElementById('eighth_label').className="bar_lower";
-
-};
-
-document.getElementById('eighth_label').onclick=function(){
-    document.getElementById('first_label').className="bar_lower";
-    document.getElementById('second_label').className="bar_lower";
-    document.getElementById('third_label').className="bar_lower";
-    document.getElementById('fourth_label').className="bar_lower";
-    document.getElementById('fiveth_label').className="bar_lower";
-    document.getElementById('sixth_label').className="bar_lower";
-    document.getElementById('seventh_label').className="bar_lower";
-    document.getElementById('eighth_label').className="bar_lower1";
-
-};
 
 document.getElementById('google_logo').onclick=function(){
-  location.href="https://grubyrubel.github.io/google_homepage";
+  location.href="index.html";
+};
+
+document.getElementById('r1').onclick=function(){
+  document.getElementById('r1').className="center_part_after";
+};
+
+document.getElementById('r2').onclick=function(){
+  document.getElementById('r2').className="center_part_after";
+};
+
+document.getElementById('r3').onclick=function(){
+  document.getElementById('r3').className="center_part_after";
+};
+
+document.getElementById('r4').onclick=function(){
+  document.getElementById('r4').className="center_part_after";
+};
+
+document.getElementById('r5').onclick=function(){
+  document.getElementById('r5').className="center_part_after";
+};
+
+document.getElementById('r6').onclick=function(){
+  document.getElementById('r6').className="center_part_after";
+};
+
+document.getElementById('r7').onclick=function(){
+  document.getElementById('r7').className="center_part_after";
+};
+
+document.getElementById('r8').onclick=function(){
+  document.getElementById('r8').className="center_part_after";
+};
+
+document.getElementById('r9').onclick=function(){
+  document.getElementById('r9').className="center_part_after";
+};
+
+document.getElementById('r10').onclick=function(){
+  document.getElementById('r10').className="center_part_after";
+};
+
+var gora=document.getElementById('nav');
+var top_nav_left=document.getElementById('top_nav_left');
+var google_logo=document.getElementById('google_logo');
+
+
+
+window.onscroll=function(){
+  
+  if (window.pageYOffset >= "118"){
+    
+    
+    gora.className="nav_hidden";
+
+    top_nav_left.className="top_nav_left1";
+    
+    google_logo.className="google_logo1";
+
+    text_field_prevent.className="text_field1";
+
+    button_clear.className="button1";
+
+    bar_clear.className="bar1";
+
+    magnifier_blue.className="magnifier_blue1";
+
+    if(magnifier.className=="magnifier-non"){
+      magnifier.className="magnifier-non1";
+      searchbox_prevent.className="searchbox-non1";
+    }else{
+      if(magnifier.className=="magnifier-non1"){
+        magnifier.className="magnifier-non1";
+        searchbox_prevent.className="searchbox-non1";
+      }else{
+        magnifier.className="magnifier1";
+        searchbox_prevent.className="searchbox1";
+      };
+    };
+
+    document.addEventListener('click', function(event) {
+      var isClickInside = text_field_prevent.contains(event.target);
+    
+      if (!isClickInside) {
+        magnifier.className="magnifier-non1";
+        searchbox_prevent.className="searchbox-non1";
+      }else{
+          magnifier.className="magnifier1";
+          searchbox_prevent.className="searchbox1";
+      }
+    });
+
+    text_field_prevent.onkeypress=function(){
+      magnifier.className="magnifier1";
+      searchbox_prevent.className="searchbox1";
+  
+    };
+
+    if(window.pageYOffset <= "183"){
+      gora.style.marginTop=window.pageYOffset + 'px';
+    }else{
+      gora.style.marginTop = "183px";
+    };
+
+    document.getElementById('top_nav_right').style.visibility="hidden";
+
+  }else{
+
+    gora.className="nav";
+
+    top_nav_left.className="top_nav_left";
+
+    google_logo.className="google_logo";
+
+    text_field_prevent.className="text_field";
+
+    button_clear.className="button";
+
+    bar_clear.className="bar";
+   
+    magnifier_blue.className="magnifier_blue";
+
+    if(magnifier.className=="magnifier-non1"){
+      magnifier.className="magnifier-non";
+      searchbox_prevent.className="searchbox-non";
+    }else{
+      if(magnifier.className=="magnifier-non"){
+        magnifier.className="magnifier-non";
+        searchbox_prevent.className="searchbox-non";
+      }else{
+        magnifier.className="magnifier";
+        searchbox_prevent.className="searchbox";
+      };
+    };
+
+    document.addEventListener('click', function(event) {
+      var isClickInside = text_field_prevent.contains(event.target);
+    
+      if (!isClickInside) {
+        magnifier.className="magnifier-non";
+        searchbox_prevent.className="searchbox-non";
+      }else{
+          magnifier.className="magnifier";
+          searchbox_prevent.className="searchbox";
+      }
+    });
+
+    text_field_prevent.onkeypress=function(){
+      magnifier.className="magnifier";
+      searchbox_prevent.className="searchbox";
+  
+    };
+
+    gora.style.marginTop="0";
+
+    document.getElementById('top_nav_right').style.visibility="visible";
+  };
+  
 };
